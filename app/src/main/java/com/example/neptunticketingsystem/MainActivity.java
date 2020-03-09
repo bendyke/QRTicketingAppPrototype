af4af993f32e521539e5dbdb3360b2586a1221af
Bendeguz Kassai <Bendo@admins-MacBook-Pro.local>
2020-03-09 10:01:09 +0100

    fixed prepString() method

--- app/src/main/java/com/example/neptunticketingsystem/MainActivity.java
+++ app/src/main/java/com/example/neptunticketingsystem/MainActivity.java
@@ -18,8 +18,6 @@ import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.neptunticketingsystem.QRText";
-    // EditText nameEditText = findViewById(R.id.nameEditText);
-    // EditText neptuncodeEditText = findViewById(R.id.neptuncodeEditText);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     public String prepString(EditText text1, EditText text2){
-        String complete = text1.getText().toString().trim() + "\n" + text2.getText().toString().trim();
-        return complete;
+        return text1.getText().toString().trim() + "\n" + text2.getText().toString().trim();
     }
     public void toQRActivityIntent(){
         EditText nameEditText = findViewById(R.id.nameEditText);
